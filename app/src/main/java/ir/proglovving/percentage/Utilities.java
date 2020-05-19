0d89dc4cf9b60bcf4a14bcf26043aaf300288874
Amin <valiamin21@gmail.com>
2020-05-19 22:37:26 +0430

    adding showErrorToast() method in Utilities class

--- app/src/main/java/ir/proglovving/percentage/Utilities.java
+++ app/src/main/java/ir/proglovving/percentage/Utilities.java
@@ -3,9 +3,14 @@ package ir.proglovving.percentage;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Typeface;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.StringRes;
+import androidx.core.content.ContextCompat;
 
 public class Utilities {
 
@@ -24,4 +29,22 @@ public class Utilities {
     public static Typeface getAppTypeface(Context context){
         return ((MyApplication)context.getApplicationContext()).getAppTypeface();
     }
+
+    public static void showErrorToast(Context context, String message, int length){
+        Toast appToast = new Toast(context);
+
+        appToast.setView(LayoutInflater.from(context).inflate(R.layout.layout_error_toast, null));
+        TextView appToastTextView = appToast.getView().findViewById(R.id.txt_message);
+        appToast.setDuration(length);
+        appToastTextView.setText(message);
+            appToastTextView.setBackgroundColor(ContextCompat.getColor(context, R.color.error_toast_background));
+
+        appToast.show();
+    }
+
+    public static void showErrorToast(Context context, @StringRes int message, int length) {
+        showErrorToast(context,context.getString(message),length);
+    }
+
+
 }
