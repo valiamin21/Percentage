a56127bb97c21e52eb164a841a8136fc6fbb2a45
Amin <valiamin21@gmail.com>
2020-05-19 22:16:47 +0430

    invalid inputs handling and adding onNumberPickerScroll() to ExamController

--- app/src/main/java/ir/proglovving/percentage/ExamController.java
+++ app/src/main/java/ir/proglovving/percentage/ExamController.java
@@ -8,18 +8,32 @@ public class ExamController {
 
     private DecimalFormat decimalFormat = new DecimalFormat("0.##");
 
-    public ExamController(ExamBaseView baseView, ExamModel model){
+    public ExamController(ExamBaseView baseView, ExamModel model) {
         this.baseView = baseView;
         this.model = model;
     }
-    public void setData(int questionNo, int rightNo, int wrongNo){
+
+    public void setData(int questionNo, int rightNo, int wrongNo) {
         model.setQuestionsNo(questionNo);
         model.setRightNo(rightNo);
         model.setWrongNo(wrongNo);
     }
 
-    public void updateView(){
-        baseView.updateUi(decimalFormat.format(model.calculateMainPercent()),decimalFormat.format(model.calculatePercentWithoutWrongs()),decimalFormat.format(model.calculatePercentIfWrongsWasRight()));
+    public void calculatePercent(boolean showErrorMessages) {
+        if (model.getQuestionsNo() < model.getRightNo() + model.getWrongNo() || model.getQuestionsNo() == 0) {
+            if (showErrorMessages)
+                baseView.showErrorToast("لطفا اطلاعات‌را درست وارد کنید!");
+            baseView.updateUi("---", "---", "---");
+        } else {
+            baseView.updateUi(decimalFormat.format(model.calculateMainPercent()) + "%"
+                    , decimalFormat.format(model.calculatePercentWithoutWrongs()) + "%"
+                    , decimalFormat.format(model.calculatePercentIfWrongsWasRight()) + "%"
+            );
+        }
+    }
+
+    public void onNumPickersScroll(){
+        baseView.updateUi("---", "---", "---");
     }
 
 }
