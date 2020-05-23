006073a48ee8a23028ee32d0ffe4281657beb892
Amin <valiamin21@gmail.com>
2020-05-23 17:51:55 +0430

    implementing bottom sheet behaviors in MainActivity

--- app/src/main/java/ir/proglovving/percentage/views/MainActivity.java
+++ app/src/main/java/ir/proglovving/percentage/views/MainActivity.java
@@ -1,25 +1,35 @@
 package ir.proglovving.percentage.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import ir.proglovving.percentage.OnBottomSheetStateChanged;
 import ir.proglovving.percentage.controllers.ExamController;
 import ir.proglovving.percentage.models.ExamModel;
 import ir.proglovving.percentage.R;
 import ir.proglovving.percentage.Utilities;
 
 public class MainActivity extends AppCompatActivity implements ExamBaseView {
-    private static final String TAG = "MainActivity";
     private NumberPicker questionsNumPicker, wrongsNumPicker, rightsNumPicker;
     private TextView mainPercentTextView, percentWithoutWrongsTextView, percentIfWrongsWasRightTextView;
     private Button calculateButton;
+    private LinearLayout bottomSheetParent;
+    private ImageButton bottomSheetOpenCloseButton;
+    private TextView bottomSheetTitle;
+    private BottomSheetBehavior bottomSheetBehavior;
 
     private ExamController controller;
 
@@ -49,6 +59,52 @@ public class MainActivity extends AppCompatActivity implements ExamBaseView {
                 controller.calculatePercent(true);
             }
         });
+
+        bottomSheetOpenCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                if(bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED){
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }else if(bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED){
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+        });
+
+        bottomSheetTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetOpenCloseButton.callOnClick();
+            }
+        });
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetParent);
+        bottomSheetBehavior.addBottomSheetCallback(new OnBottomSheetStateChanged() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        rotateBottomSheetOpenCloseImage(180);
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        rotateBottomSheetOpenCloseImage(0);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void rotateBottomSheetOpenCloseImage(float endAngle){
+        float startAngle = bottomSheetOpenCloseButton.getRotation();
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(startAngle, endAngle);
+        valueAnimator.setDuration(150);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                bottomSheetOpenCloseButton.setRotation((float) animation.getAnimatedValue());
+            }
+        });
+        valueAnimator.start();
     }
 
     private void setupViews() {
@@ -70,6 +126,13 @@ public class MainActivity extends AppCompatActivity implements ExamBaseView {
         percentIfWrongsWasRightTextView = findViewById(R.id.tv_percentIfWrongsWasRight);
 
         calculateButton = findViewById(R.id.btn_calculate);
+
+        bottomSheetParent = findViewById(R.id.bottom_sheet_patent);
+        // Setting onclickListener as null prevents user interacts with underlying views
+        bottomSheetParent.setOnClickListener(null);
+
+        bottomSheetOpenCloseButton = findViewById(R.id.imgBtn_close_open_bottom_sheet);
+        bottomSheetTitle = findViewById(R.id.txt_bottomSheetTitle);
     }
 
     private void setupNumPickers(NumberPicker numberPicker) {
@@ -90,6 +153,6 @@ public class MainActivity extends AppCompatActivity implements ExamBaseView {
 
     @Override
     public void showErrorToast(String message) {
-        Utilities.showErrorToast(this,message,Toast.LENGTH_SHORT);
+        Utilities.showErrorToast(this, message, Toast.LENGTH_SHORT);
     }
 }
