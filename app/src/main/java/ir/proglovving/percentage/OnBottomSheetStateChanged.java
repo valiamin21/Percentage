4366c15b4c18fb35df6264cec578b0784ba3bdc1
Amin <valiamin21@gmail.com>
2020-05-23 17:51:33 +0430

    making OnBottomSheetStateChanged abstract class

---
+++ app/src/main/java/ir/proglovving/percentage/OnBottomSheetStateChanged.java
@@ -0,0 +1,20 @@
+package ir.proglovving.percentage;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public abstract class OnBottomSheetStateChanged extends BottomSheetBehavior.BottomSheetCallback {
+
+    @Override
+    public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+    }
+
+    @Override
+    public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+    }
+}
