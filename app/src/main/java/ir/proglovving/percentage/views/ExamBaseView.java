50d7724ce700555f5d62bcc6965433becee93631
Amin <valiamin21@gmail.com>
2020-05-24 12:54:16 +0430

    updating EamBaseView

--- app/src/main/java/ir/proglovving/percentage/views/ExamBaseView.java
+++ app/src/main/java/ir/proglovving/percentage/views/ExamBaseView.java
@@ -1,6 +1,12 @@
 package ir.proglovving.percentage.views;
 
+import androidx.annotation.StringRes;
+
 public interface ExamBaseView {
     void updateUi(String mainPercent, String percentWithoutWrongs, String percentIfWrongsWasRight);
     void showErrorToast(String message);
+    void showErrorToast(@StringRes int message);
+    void dismissDialog();
+    void clearDialog();
+    void refreshExamHistoryRecyclerView();
 }
