1b9c55a5e2ff894d69b33d42fbe5ae599a135771
Amin <valiamin21@gmail.com>
2020-05-24 12:47:57 +0430

    making ExamHistoryModel class

---
+++ app/src/main/java/ir/proglovving/percentage/models/ExamHistoryModel.java
@@ -0,0 +1,13 @@
+package ir.proglovving.percentage.models;
+
+public class ExamHistoryModel extends ExamModel{
+    private String examName;
+
+    public void setExamName(String examName){
+        this.examName = examName;
+    }
+
+    public String getExamName(){
+        return examName;
+    }
+}
